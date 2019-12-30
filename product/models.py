83212d57db519ccf0da7fe45e1ebe39a8a46b8b3
nameunji <namji117@gmail.com>
2019-12-30 21:29:48 +0900

    product/product, product_info, product_sub_images modelling

--- product/models.py
+++ product/models.py
@@ -21,3 +21,42 @@ class Brands(models.Model):
     detail_image     = models.URLField(max_length = 2000, null=True)
     class Meta:
         db_table = 'brands'
+
+
+class Products(models.Model):
+    main_category    = models.ForeignKey(ProductMainCategories, on_delete=models.CASCADE)
+    sub_category     = models.ForeignKey(ProductSubCategories, on_delete=models.CASCADE)
+    brand            = models.ForeignKey(Brands, on_delete=models.CASCADE)
+    name             = models.CharField(max_length = 100)
+    price            = models.DecimalField(max_digits=11, decimal_places=2)
+    minimum_count    = models.SmallIntegerField()
+    maximum_count    = models.SmallIntegerField()
+    start_date       = models.DateTimeField()
+    end_date         = models.DateTimeField()
+    delivery_date    = models.DateField()
+    delivery_charge  = models.DecimalField(max_digits=9, decimal_places=2)
+    detail_content   = models.TextField()
+    info_detail      = models.TextField()
+
+    class Meta:
+        db_table = 'products'
+
+
+class ProductInfo(models.Model):
+    product      = models.ForeignKey(Products, on_delete=models.CASCADE)
+    artist       = models.CharField(max_length = 50, null=True)
+    main_text    = models.CharField(max_length = 1000, null=True)
+    md_name      = models.CharField(max_length = 50, null=True)
+    main_image   = models.URLField(max_length = 2000, null=True)
+    new_image    = models.URLField(max_length = 2000, null=True)
+    search_image = models.URLField(max_length = 2000, null=True)
+    class Meta:
+        db_table = 'product_info'
+
+
+class ProductSubImages(models.Model):
+    product    = models.ForeignKey(Products, on_delete=models.CASCADE)
+    sub_image  = models.URLField(max_length = 2000, null=True)
+    class Meta:
+        db_table = 'product_sub_images'
+
