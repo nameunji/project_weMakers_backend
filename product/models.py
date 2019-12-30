53490c386b8d64c6077cf860a8abc1eda1ae587d
nameunji <namji117@gmail.com>
2019-12-30 16:41:53 +0900

    product / update model_column name

--- product/models.py
+++ product/models.py
@@ -8,13 +8,13 @@ class ProductMainCategories(models.Model):
 
 class ProductSubCategories(models.Model):
     name             = models.CharField(max_length = 50)
-    main_category_id = models.ForeignKey(ProductMainCategories, on_delete=models.CASCADE)
+    main_category    = models.ForeignKey(ProductMainCategories, on_delete=models.CASCADE)
     class Meta:
         db_table = 'product_sub_categories'
 
 
 class Brands(models.Model):
-    main_category_id = models.ForeignKey(ProductMainCategories, on_delete=models.CASCADE)
+    main_category    = models.ForeignKey(ProductMainCategories, on_delete=models.CASCADE)
     name             = models.CharField(max_length = 100)
     logo_image       = models.URLField(max_length = 2000, null=True)
     info             = models.CharField(max_length = 1000, null=True)
