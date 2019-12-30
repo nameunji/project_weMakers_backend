5d0da1add8f902be4e7ef81037658523eb1964d7
nameunji <namji117@gmail.com>
2019-12-30 16:28:12 +0900

    user/ add AuthView, connect url

--- user/views.py
+++ user/views.py
@@ -41,4 +41,25 @@ class UserView(View):
         except IntegrityError:
             return JsonResponse({'message':'EXCEPTED_DATA'}, status= 401)
 
-
+class AuthView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        check_password = re.compile("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$")
+        try:
+            validate_email(data["email"])
+            user = Users.objects.get(email = data["email"])
+            if not check_password.match(data["password"]) :
+                return JsonResponse({'message':'UNSATISFIED_PASSWORD'}, status = 400)
+            if bcrypt.checkpw(data["password"].encode('utf-8'), user.password.encode('utf-8')):
+                access_token = jwt.encode({'id':user.id}, SECRET_KEY, algorithm = 'HS256')
+                return JsonResponse({'access_token':access_token.decode('utf-8')}, status = 200)
+            else:
+                return JsonResponse({'message':'INVALID_PASSWORD'}, status = 401)
+        except Users.DoesNotExist:
+            return JsonResponse({'message':'INVALID_USER'}, status = 400)
+        except KeyError:
+            return JsonResponse({'message':'INVALID_KEYS'}, status = 400)
+        except ValidationError:
+            return JsonResponse({'message': 'NOT_EMAIL'}, status = 400)
+        except TypeError:
+            return JsonResponse({'message': 'INVALID_VALUE'}, status = 400)
